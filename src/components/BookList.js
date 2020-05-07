dc6c78a0d1ee293df95ec01b5528da431d3bd5ed
myungho <skqoaudgh2@hanmail.net>
2020-05-07 16:34:12 +0900

    도서 목록 표시. 클릭 시, 도서가 삭제되도록

---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+  const { books, addBook, removeBook } = useContext(BookContext);
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map((book) => (
+          <BookDetails key={book.id} book={book} />
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty">현재 읽을 도서가 없습니다. 자유시간!</div>
+  );
+};
+
+export default BookList;
